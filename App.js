89868fbcfecda1d3ae847f7139151fd788186ae9
Luv Gangwani <luv.gangwani@gmail.com>
2021-04-18 20:02:05 +1000

    Added the landing page: 	- Added :Register" and "Login" buttons

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+import Landing from './components/auth/Landing';
 
 export default function App() {
+  const { Navigator, Screen } = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator initialRouteName="Landing">
+        <Screen name="Landing" component={Landing} options={{ headerShown: false }} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
