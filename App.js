d56241e0ff0e75b3c78d5d22c68a6e32d2914b5c
Luv Gangwani <luv.gangwani@gmail.com>
2021-05-23 18:33:28 +1000

    - Register firebase auth state listener - Add the loaded and loggedIn states

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'
 import Constants from 'expo-constants';
@@ -6,36 +6,84 @@ import firebase from 'firebase';
 import Landing from './components/auth/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import { StyleSheet, Text, View } from 'react-native';
+export class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default function App() {
-  // initialize firebase app
-  const {
-    apiKey,
-    authDomain,
-    projectId,
-    storageBucket,
-    messagingSenderId,
-    appId,
-  } = Constants.manifest.extra;
+    this.state = {
+      loaded: false,
+      loggedIn: false,
+    }
+  }
 
-  if (firebase.apps.length == 0)
-    firebase.initializeApp({
+  componentDidMount() {
+    // Register auth state change listener
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user)
+        this.setState({
+          loggedIn: true
+        });
+    });
+    this.setState({
+      loaded: true,
+    });
+  }
+  
+  render() {
+    const { loaded, loggedIn } = this.state;
+    const { appContainer } = styles;
+    // initialize firebase app
+    const {
       apiKey,
       authDomain,
       projectId,
       storageBucket,
       messagingSenderId,
       appId,
-    });
+    } = Constants.manifest.extra;
 
-  const { Navigator, Screen } = createStackNavigator();
-  return (
-    <NavigationContainer>
-      <Navigator initialRouteName="Landing">
-        <Screen name="Landing" component={Landing} options={{ headerShown: false }} />
-        <Screen name="Register" component={Register} options={{ headerShown: false }} />
-        <Screen name="Login" component={Login} options={{ headerShown: false }}/>
-      </Navigator>
-    </NavigationContainer>
-  );
-};
+    if (firebase.apps.length == 0)
+      firebase.initializeApp({
+        apiKey,
+        authDomain,
+        projectId,
+        storageBucket,
+        messagingSenderId,
+        appId,
+      });
+
+      let renderJSX =   <View style={appContainer}>
+                          <Text>Loading...</Text>
+                        </View>;
+      if (loaded) {
+        if (loggedIn)
+          renderJSX = <View style={appContainer}>
+                        <Text>User logged in!</Text>
+                      </View>
+        else {
+          const { Navigator, Screen } = createStackNavigator();
+          renderJSX = <NavigationContainer>
+                        <Navigator initialRouteName="Landing">
+                          <Screen name="Landing" component={Landing} options={{ headerShown: false }} />
+                          <Screen name="Register" component={Register} options={{ headerShown: false }} />
+                          <Screen name="Login" component={Login} options={{ headerShown: false }}/>
+                        </Navigator>
+                      </NavigationContainer>
+        }
+      }
+
+    
+    return renderJSX;
+  }
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+      flex: 1,
+      justifyContent: 'center',
+  }
+});
+
+
+export default App
