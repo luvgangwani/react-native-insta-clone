6bd526767b6a136a2a06f3de4a693a098ce91ea4
Luv Gangwani <luvgangwani>
2021-06-01 00:14:53 +1000

    Add a functionality to take a picture

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,7 +1,7 @@
 import { Camera } from 'expo-camera';
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, Image, StyleSheet, Text, View } from 'react-native'
 
 // destructure the camera types (back camera/front camera)
 const { back, front } = Camera.Constants.Type;
@@ -9,8 +9,13 @@ const { back, front } = Camera.Constants.Type;
 function Add() {
     // tracks camera permissions
     const [hasPermission, setHasPermission] = useState(null);
+    // track the camera type (back camera/front camera)
     // initial state is set to launch the back camera
     const [type, setType] = useState(back);
+    // track if the camera reference is set
+    const [camera, setCamera] = useState(null);
+    // track the image uri
+    const [image, setImage] = useState(null);
 
     // check that the user has granted permissions to use the Camera when component mounts
     useEffect(() => {
@@ -23,15 +28,24 @@ function Add() {
     //handler function to flip the camera
     const flipCamera = () => {
         setType(type === back ? front : back)
-    }
+    };
+
+    // handler function to take a picture
+    const takePicture = async () => {
+        if (camera) {
+            const { uri } = await camera.takePictureAsync(null);
+            setImage(uri);
+        }
+    };
 
     // rendering logic
 
-    const { cameraContainer, camera } = styles;
+    const { cameraContainer, camera: camStyle } = styles;
     let renderJSX = <View style={{ flex: 1}}>
                         <View style={cameraContainer}>
                             <Camera
-                                style={camera}
+                                ref = {(ref) => setCamera(ref)}
+                                style={camStyle}
                                 type={type}
                                 ratio={'1:1'}
                             />
@@ -40,6 +54,11 @@ function Add() {
                             title="Flip"
                             onPress={flipCamera}
                         ></Button>
+                        <Button
+                            title="Take picture"
+                            onPress={takePicture}
+                        ></Button>
+                        { image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
                     </View>;
 
     if (!hasPermission) renderJSX = <Text>No access to camera</Text>;
