3fbc950152bbeafc011fee2c5a93ce6d701f69f3
Luv Gangwani <luvgangwani>
2021-07-04 19:59:26 +1000

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,68 +1,21 @@
-import React, { Component } from 'react'
-import Constants from 'expo-constants';
-import firebase from 'firebase';
-import { Provider } from 'react-redux';
-import { LogBox, StyleSheet, Text, View } from 'react-native';
-import store from './redux/store';
-import Init from './Init';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-LogBox.ignoreLogs(['Setting a timer']);
-
-export class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loaded: false,
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      loaded: true,
-    });
-  }
-
-  render() {
-    const { loaded } = this.state;
-    const { appContainer } = styles;
-    // initialize firebase app
-    const {
-      apiKey,
-      authDomain,
-      projectId,
-      storageBucket,
-      messagingSenderId,
-      appId,
-    } = Constants.manifest.extra;
-
-    if (firebase.apps.length == 0)
-      firebase.initializeApp({
-        apiKey,
-        authDomain,
-        projectId,
-        storageBucket,
-        messagingSenderId,
-        appId,
-      });
-      // when the component is not yet mounted
-      let renderJSX =   <View style={appContainer}>
-                          <Text>Loading...</Text>
-                        </View>;
-      if (loaded) {
-        renderJSX = <Init />
-      }
-
-    
-    return <Provider store={store}>{renderJSX}</Provider>;
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-      flex: 1,
-      justifyContent: 'center',
-  }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
-
-export default App
